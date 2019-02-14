ed9191065f42293d455cc2d6953d182e6fe90a9e
Hao <zhaozilanya@gmail.com>
2019-02-14 02:54:43 -0800

    fix bug

--- IAT339_P02/js/scrpitV2.js
+++ IAT339_P02/js/scrpitV2.js
@@ -158,4 +158,26 @@ $('.multi-item-carousel .item').each(function(){
   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
-  
+//product list https://codepen.io/mmcshinsky/pen/ogHxi
+function checkWindowSize() {
+  if ( jQuery(window).width() >= 480 ) {
+      $('.truncate').succinct({
+          size: 100
+      }); 
+  }
+  else if ( jQuery(window).width() >= 320 ) {
+      $('.truncate').succinct({
+          size: 55
+      }); 
+  } 
+  else {
+      $('.truncate').succinct({
+          size: 150
+      }); 
+  }   
+}
+
+jQuery(document).ready(function(){
+  jQuery(window).resize(checkWindowSize);
+  checkWindowSize();
+});
